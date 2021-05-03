3b19b31ea8c799d2a6a7ad3d3be9c7382a4bf23e
Maiwen26 <mmaginet@ensc.fr>
2021-05-03 17:11:22 +0200

    j'ajoute mes fichiers

---
+++ MondePoulpe/Core/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondePoulpe.Core
+{
+    public class Player : Actor
+    {
+        public Player()
+        {
+            Awareness = 15;
+            Name = "Poulpe";
+            Color = Colors.Player;
+            Symbol = '@';
+            X = 10;
+            Y = 10;
+        }
+    }
+}
+
